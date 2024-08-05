0afb3448bb5523142c0e0a28e98061260e81032f
arjunhm <hm.arjun97@gmail.com>
2024-08-05 18:31:59 -0400

    defined structs

---
+++ cache.h
@@ -0,0 +1,22 @@
+#include <inttypes.h>
+#include <stdbool.h>
+
+#define u8 uint8_t
+#define u32 uint32_t
+
+typedef struct Line {
+	u8 tag;
+	bool valid;
+}
+
+typedef struct Set {
+	Line *lines;
+	u32 l;
+} Set;
+
+typedef struct Cache {
+	u32 s;  
+	Set *sets;
+} Cache;
+
+
